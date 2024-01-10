cf24aea633d0d69ddbf7194749d1adfd0da8e917
chiu <weixiaosmile010203@outlook.com>
2024-01-10 17:47:38 +0800

    添加内存百分比图

--- memorys.py
+++ memorys.py
@@ -20,36 +20,81 @@ for timestamp in timestamps:
 memfree = []
 memused = []
 memavail = []
+memused_percent = []
 for memory in memorys:
     memfree.append(memory['memfree'])
     memused.append(memory['memused'])
-
+    memavail.append(memory['avail'])
+    memused_percent.append(memory['memused-percent'])
 print(len(timestamp_list))
 print(len(memfree))
 
 
 import pyecharts.options as opts
-from pyecharts.charts import Line
+from pyecharts.charts import Line, Grid
 from pyecharts.faker import Faker
 
 
+# 创建内存使用率折线图表
 c = (
     Line()
     .add_xaxis(timestamp_list)
-    .add_yaxis("free", memfree, is_smooth=True)
-    .add_yaxis("used", memused, is_smooth=True)
+    .add_yaxis("memused_percent", memused_percent, is_smooth=True)
     .set_series_opts(
         areastyle_opts=opts.AreaStyleOpts(opacity=0.5),
         label_opts=opts.LabelOpts(is_show=False),
     )
     .set_global_opts(
-        title_opts=opts.TitleOpts(title="内存使用"),
+        title_opts=opts.TitleOpts(title="内存使用率", pos_top="5%"),
+        legend_opts=opts.LegendOpts(pos_top="5%"),
         xaxis_opts=opts.AxisOpts(
             axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
             is_scale=False,
             boundary_gap=False,
         ),
-        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100)],
+        # brush_opts 配置用于设置 datazoom 的相关参数
+        brush_opts=opts.BrushOpts(
+            x_axis_index=0,
+            brush_link="memory_brush",  # 设置 brush_link，确保两个图形联动
+            brush_type="lineX",  # "lineX" 表示横向的 datazoom
+        ),
+        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="1%")],
     )
-    .render("line_areastyle_boundary_gap.html")
 )
+
+# 创建内存使用折线图表
+c2 = (
+    Line()
+    .add_xaxis(timestamp_list)
+    .add_yaxis("free", memfree, is_smooth=True)
+    .add_yaxis("used", memused, is_smooth=True)
+    .add_yaxis("avail", memavail, is_smooth=True)
+    .set_series_opts(
+        areastyle_opts=opts.AreaStyleOpts(opacity=0.5),
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="内存使用", pos_top="50%"),
+        legend_opts=opts.LegendOpts(pos_top="50%"),
+        xaxis_opts=opts.AxisOpts(
+            axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
+            is_scale=False,
+            boundary_gap=False,
+        ),
+        # brush_opts 配置用于设置 datazoom 的相关参数
+        brush_opts=opts.BrushOpts(
+            x_axis_index=0,
+            brush_link="memory_brush",  # 设置 brush_link，确保两个图形联动
+            brush_type="lineX",  # "lineX" 表示横向的 datazoom
+        ),
+        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="20%")],
+    )
+)
+
+# 创建两个 Grid 实例，设置纵向排列
+grid = Grid(init_opts=opts.InitOpts(width="1000px", height="800px"))
+grid.add(c, grid_opts=opts.GridOpts(pos_top="10%", pos_bottom="60%"))
+grid.add(c2, grid_opts=opts.GridOpts(pos_top="70%", pos_bottom="10%"))
+
+# 使用 render 方法保存为 HTML 文件
+grid.render("memory.html")
