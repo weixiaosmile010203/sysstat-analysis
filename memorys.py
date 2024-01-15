bbca42e58d6150b27175d8043d8e07baa191a568
chiu <weixiaosmile010203@outlook.com>
2024-01-15 17:09:28 +0800

    内存基础指标完成

--- memorys.py
+++ memorys.py
@@ -1,7 +1,15 @@
 import pyecharts.options as opts
 from pyecharts.charts import Line, Page
+from pyecharts.commons.utils import JsCode
 import json
 import os
+
+def format_memory(mem_size):
+    if mem_size < 1e3:  # 小于1GB，以MB为单位显示
+        return f"{mem_size:.2f} MB"
+    else:  # 大于等于1GB，以GB为单位显示
+        return f"{mem_size / 1e3:.2f} GB"
+    
 
 data = json.load(open(os.path.join(os.path.dirname(__file__), 'sa01.json')))
 # 获取时间戳列表
@@ -13,6 +21,7 @@ for host in data['sysstat']['hosts']:
     for statistic in host['statistics']:
         timestamps.append(statistic['timestamp'])
         memorys.append(statistic['memory'])
+
 timestamp_list = []
 for timestamp in timestamps:
     timestamp_list.append(timestamp.get('date') + ' ' + timestamp.get('time'))
@@ -21,80 +30,139 @@ memfree = []
 memused = []
 memavail = []
 memused_percent = []
+memcache = []
+membuffer = []
+memswpfree = []
+memswpused = []
+memswpused_percent = []
+# memswpcad = []
+# memswpcad_percent = []
+
+
 for memory in memorys:
     memfree.append(memory['memfree'])
     memused.append(memory['memused'])
     memavail.append(memory['avail'])
     memused_percent.append(memory['memused-percent'])
-print(len(timestamp_list))
-print(len(memfree))
-
-
-import pyecharts.options as opts
-from pyecharts.charts import Line, Grid
-from pyecharts.faker import Faker
-
+    memcache.append(memory['cached'])
+    membuffer.append(memory['buffers'])
+    memswpfree.append(memory['swpfree'])
+    memswpused.append(memory['swpused'])
+    memswpused_percent.append(memory['swpused-percent'])
+    # memswpcad.append(memory['swpcad'])
+    # memswpcad_percent.append(memory['swpcad-percent'])
 
 # 创建内存使用率折线图表
-c = (
+mem_user_percent = (
     Line()
     .add_xaxis(timestamp_list)
     .add_yaxis("memused_percent", memused_percent, is_smooth=True)
     .set_series_opts(
-        areastyle_opts=opts.AreaStyleOpts(opacity=0.5),
+        areastyle_opts=opts.AreaStyleOpts(opacity=0.2),
         label_opts=opts.LabelOpts(is_show=False),
     )
     .set_global_opts(
-        title_opts=opts.TitleOpts(title="内存使用率", pos_top="5%"),
-        legend_opts=opts.LegendOpts(pos_top="5%"),
+        title_opts=opts.TitleOpts(title="内存使用率", pos_top="1%"),
+        legend_opts=opts.LegendOpts(pos_top="1%"),
         xaxis_opts=opts.AxisOpts(
             axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
             is_scale=False,
             boundary_gap=False,
         ),
+        yaxis_opts=opts.AxisOpts(max_=100),
         # brush_opts 配置用于设置 datazoom 的相关参数
-        brush_opts=opts.BrushOpts(
-            x_axis_index=0,
-            brush_link="memory_brush",  # 设置 brush_link，确保两个图形联动
-            brush_type="lineX",  # "lineX" 表示横向的 datazoom
+        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="1%")],
+    )
+)
+
+mem_use = (
+    Line()
+    .add_xaxis(timestamp_list) 
+    .add_yaxis("mem_used", memused, is_smooth=True)
+    .add_yaxis("mem_avail", memavail, is_smooth=True)
+    .add_yaxis("mem_free", memfree, is_smooth=True)
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="已使用内存", pos_top="1%"),
+        legend_opts=opts.LegendOpts(pos_top="1%"),
+        xaxis_opts=opts.AxisOpts(
+            axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
+            is_scale=False,
+            boundary_gap=False,
         ),
         datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="1%")],
     )
 )
 
-# 创建内存使用折线图表
-c2 = (
+mem_cache = (
     Line()
-    .add_xaxis(timestamp_list)
-    .add_yaxis("free", memfree, is_smooth=True)
-    .add_yaxis("used", memused, is_smooth=True)
-    .add_yaxis("avail", memavail, is_smooth=True)
+    .add_xaxis(timestamp_list) 
+    .add_yaxis("mem_used", memused, is_smooth=True)
+    .add_yaxis("mem_buffer", membuffer, is_smooth=True)
+    .add_yaxis("mem_cached", memcache, is_smooth=True)
     .set_series_opts(
         areastyle_opts=opts.AreaStyleOpts(opacity=0.5),
         label_opts=opts.LabelOpts(is_show=False),
     )
     .set_global_opts(
-        title_opts=opts.TitleOpts(title="内存使用", pos_top="50%"),
-        legend_opts=opts.LegendOpts(pos_top="50%"),
+        title_opts=opts.TitleOpts(title="缓存", pos_top="1%"),
+        legend_opts=opts.LegendOpts(pos_top="1%"),
         xaxis_opts=opts.AxisOpts(
             axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
             is_scale=False,
             boundary_gap=False,
         ),
-        # brush_opts 配置用于设置 datazoom 的相关参数
-        brush_opts=opts.BrushOpts(
-            x_axis_index=0,
-            brush_link="memory_brush",  # 设置 brush_link，确保两个图形联动
-            brush_type="lineX",  # "lineX" 表示横向的 datazoom
-        ),
-        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="20%")],
+        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="1%")],
     )
 )
 
-# 创建两个 Grid 实例，设置纵向排列
-grid = Grid(init_opts=opts.InitOpts(width="1000px", height="800px"))
-grid.add(c, grid_opts=opts.GridOpts(pos_top="10%", pos_bottom="60%"))
-grid.add(c2, grid_opts=opts.GridOpts(pos_top="70%", pos_bottom="10%"))
+mem_swap_percent = (
+    Line()
+    .add_xaxis(timestamp_list)
+    .add_yaxis("swap使用率", memswpused_percent, is_smooth=True)
+    .set_series_opts(
+        areastyle_opts=opts.AreaStyleOpts(opacity=0.2),
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="swap使用率", pos_top="1%"),
+        legend_opts=opts.LegendOpts(pos_top="1%"),
+        xaxis_opts=opts.AxisOpts(
+            axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
+            is_scale=False,
+            boundary_gap=False,
+        ),
+        yaxis_opts=opts.AxisOpts(max_=100),
+        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="1%")],
+    )
+)
 
-# 使用 render 方法保存为 HTML 文件
-grid.render("memory.html")
+mem_swap_used = (
+    Line()
+    .add_xaxis(timestamp_list)
+    .add_yaxis("swap使用", memswpused_percent, is_smooth=True, stack="swap_total")
+    .add_yaxis("swap剩余", memswpfree, is_smooth=True, stack="swap_total")
+    .set_series_opts(
+        areastyle_opts=opts.AreaStyleOpts(opacity=0.2),
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="swap使用率", pos_top="1%"),
+        legend_opts=opts.LegendOpts(pos_top="1%"),
+        xaxis_opts=opts.AxisOpts(
+            axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
+            is_scale=False,
+            boundary_gap=False,
+        ),
+        datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100, pos_bottom="1%")],
+    )
+)
+
+
+page = Page()
+page.page_title = "内存详情"
+page.add(mem_user_percent)
+page.add(mem_use)
+page.add(mem_cache)
+page.add(mem_swap_percent)
+page.add(mem_swap_used)
+page.render("memory.html")
